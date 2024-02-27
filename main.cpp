031971fd2230d9d60ad3cd8142618b5934177c25
Likstor <94634271+Likstor@users.noreply.github.com>
2024-02-27 20:46:51 +0300

    polish notation

--- main.cpp
+++ main.cpp
@@ -5,29 +5,35 @@
 #include <unordered_map>
 #include <vector>
 #include <miniCLexer/lexer.h>
-
+#include <sym.h>
 
 int main(int argc, char *argv[])
 {
-    if (argc < 1)
-    {
-        std::cerr << "None file path";
-        return -1;
-    }
+    // if (argc < 1)
+    // {
+    //     std::cerr << "None file path";
+    //     return -1;
+    // }
 
     std::ifstream stream(argv[1], std::ios::in);
 
-    miniCLexer::Lexer lexer(stream);
+    miniCLexer::Lexer lexer{stream};
 
-    while (true)
+    // while (true)
+    // {
+    //     miniCLexer::Token temp = lexer.getNextLexem();
+
+    //     std::cout << "[" << temp.token << ", \"" << temp.value << "\"]" << std::endl;
+
+    //     if (temp.token == "error" || temp.token == "eof")
+    //     {
+    //         break;
+    //     }
+    // }
+
+    ShuntingYardMachine sym(lexer);
+    for (miniCLexer::Token temp : sym.getReversePolishNotation())
     {
-        miniCLexer::Token temp = lexer.getNextLexem();
-
-        std::cout << "[" << temp.token << ", \"" << temp.value << "\"]" << std::endl;
-
-        if (temp.token == "error" || temp.token == "eof")
-        {
-            break;
-        }
+        std::cout << temp.token << " " << temp.value << std::endl;
     }
 }
