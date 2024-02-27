b22bd1746c729ed16918d1411a4388b2bfa28382
Likstor <94634271+Likstor@users.noreply.github.com>
2024-02-27 20:56:46 +0300

    base ver

--- main.cpp
+++ main.cpp
@@ -3,37 +3,29 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
-#include <vector>
 #include <miniCLexer/lexer.h>
-#include <sym.h>
 
 int main(int argc, char *argv[])
 {
-    // if (argc < 1)
-    // {
-    //     std::cerr << "None file path";
-    //     return -1;
-    // }
+    if (argc < 1)
+    {
+        std::cerr << "None file path";
+        return -1;
+    }
 
     std::ifstream stream(argv[1], std::ios::in);
 
     miniCLexer::Lexer lexer{stream};
 
-    // while (true)
-    // {
-    //     miniCLexer::Token temp = lexer.getNextLexem();
-
-    //     std::cout << "[" << temp.token << ", \"" << temp.value << "\"]" << std::endl;
-
-    //     if (temp.token == "error" || temp.token == "eof")
-    //     {
-    //         break;
-    //     }
-    // }
-
-    ShuntingYardMachine sym(lexer);
-    for (miniCLexer::Token temp : sym.getReversePolishNotation())
+    while (true)
     {
-        std::cout << temp.token << " " << temp.value << std::endl;
+        miniCLexer::Token temp = lexer.getNextLexem();
+
+        std::cout << "[" << temp.token << ", \"" << temp.value << "\"]" << std::endl;
+
+        if (temp.token == "error" || temp.token == "eof")
+        {
+            break;
+        }
     }
 }
