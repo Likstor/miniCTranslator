2bc783b4f8e3f3dc5fc292978e0a21ea1286b5ce
Likstor <94634271+Likstor@users.noreply.github.com>
2024-02-28 08:15:29 +0300

    change deque to stack

--- sources/sym.cpp
+++ sources/sym.cpp
@@ -2,15 +2,15 @@
 #include <sym.h>
 #include <miniCLexer/lexer.h>
 #include <string>
-#include <deque>
+#include <stack>
 #include <vector>
 #include <iostream>
 
-ShuntingYardMachine::ShuntingYardMachine(miniCLexer::Lexer &lexer): lexer{lexer} {};
+ShuntingYardMachine::ShuntingYardMachine(miniCLexer::Lexer &lexer) : lexer{lexer} {};
 
-std::vector<miniCLexer::Token> ShuntingYardMachine::getReversePolishNotation() 
+std::vector<miniCLexer::Token> ShuntingYardMachine::getReversePolishNotation()
 {
-    std::deque<miniCLexer::Token> stack;
+    std::stack<miniCLexer::Token> stack;
     std::vector<miniCLexer::Token> output;
 
     miniCLexer::Token lexeme;
@@ -18,65 +18,70 @@ std::vector<miniCLexer::Token> ShuntingYardMachine::getReversePolishNotation()
     {
         lexeme = lexer.getNextLexem();
 
-        if (lexeme.token == "eof") {
+        if (lexeme.token == "eof")
+        {
             break;
         }
 
-        if (lexeme.token == "num") 
+        if (lexeme.token == "num")
         {
             output.push_back(lexeme);
         }
         else if (lexeme.token == "lpar")
         {
-            stack.push_back(lexeme);
+            stack.push(lexeme);
         }
         else if (lexeme.token == "rpar")
         {
-            while (stack.back().token != "lpar") {
-                output.push_back(stack.back());
-                stack.pop_back();
+            while (stack.top().token != "lpar")
+            {
+                output.push_back(stack.top());
+                stack.pop();
 
                 if (stack.empty())
                 {
                     return {{"error", ""}};
                 }
             }
-            stack.pop_back();
+            stack.pop();
         }
         else
         {
-            if (stack.size() == 0 || stack.back().token == "lpar") 
+            if (stack.size() == 0 || stack.top().token == "lpar")
             {
-                stack.push_back(lexeme);
+                stack.push(lexeme);
             }
-            else if (operators[stack.back().token].priority < operators[lexeme.token].priority) 
+            else if (operators[stack.top().token].priority < operators[lexeme.token].priority)
             {
-                stack.push_back(lexeme);
+                stack.push(lexeme);
             }
             else
             {
-                while (operators[stack.back().token].priority > operators[lexeme.token].priority ||
-                    (operators[stack.back().token].priority == operators[lexeme.token].priority && 
-                    operators[stack.back().token].associative == "LEFT")) {
-                    if (stack.back().token == "lpar") {
+                while (operators[stack.top().token].priority > operators[lexeme.token].priority ||
+                    (operators[stack.top().token].priority == operators[lexeme.token].priority &&
+                        operators[stack.top().token].associative == "LEFT"))
+                {
+                    if (stack.top().token == "lpar")
+                    {
                         break;
                     }
-                    output.push_back(stack.back());
-                    stack.pop_back();
+                    output.push_back(stack.top());
+                    stack.pop();
 
                     if (stack.empty())
                     {
                         break;
                     }
                 }
-                stack.push_back(lexeme);
+                stack.push(lexeme);
             }
         }
     }
-    
-    while (!stack.empty()) {
-        miniCLexer::Token temp = stack.back();
-        stack.pop_back();
+
+    while (!stack.empty())
+    {
+        miniCLexer::Token temp = stack.top();
+        stack.pop();
         if (temp.token == "lpar")
         {
             return {{"error", ""}};
